3e47de19d88882b03b7e7ca9cdfa70386e2b92c8
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-14 12:50:03 -0700

    feat: add transform_reviews_photos.js with mongosh commands to update the reviews_photos collection.

---
+++ db/transform/transform_reviews_photos.js
@@ -0,0 +1,79 @@
+// const fs = require('fs');
+// const path = require('path');
+// const csv = require('fast-csv');
+
+// console.log(__dirname);
+// // console.log(path.resolve(__dirname, '../../data', 'reviews_photos_test.csv'));
+
+// // Parse the test data
+// fs.createReadStream(path.resolve(__dirname, '../../data', 'reviews_photos_test.csv'))
+//     .pipe(csv.parse({ headers: true }))
+//     .on('error', error => console.error(error))
+//     .on('data', row => console.log(row))
+//     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+
+
+
+  // Put id and url fields instead photos object
+  db.reviews_photos_test.aggregate(
+    [
+      {
+        $addFields:
+          {"photo":
+            {
+              id: "$id",
+              url: "$url"
+            }
+          }
+      },
+
+      {
+        $out: "reviews_photos_test_transformed"
+      }
+    ]
+  );
+
+  // Remove unnecessary fields
+  db.reviews_photos_test_transformed.updateMany(
+    {},
+    {
+      "$unset": {"id": 1, "url": 1}
+    }
+
+  );
+
+  // convert photos object to array of review id
+  // Set review_id as _id
+  db.reviews_photos_test_transformed.aggregate(
+    [
+      {
+        $group:
+          {
+            _id: "$review_id",
+            photos: { $addToSet: "$photo"}
+          }
+      },
+
+      // {
+      //   $sort: { "photos": 1}
+      // },
+
+      {
+        $out: "reviews_photos_test_transformed"
+      }
+    ]
+  );
+
+  // db.reviews_photos_test_transformed.aggregate(
+  //   [
+  //     {
+  //       $sortArray:
+  //       {
+  //         input: "$photos",
+  //         sortBy: {id: 1}
+  //       }
+  //     }
+  //   ]
+  // );
+
+  db.reviews_photos_test_transformed.find({_id: 5});
