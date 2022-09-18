0b1fdb90d4313cbc0b327faaf4986e995611fffe
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-18 16:27:50 -0700

    feat: server routes requests to get reveiws from the database.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const mongoose = require('mongoose');
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+// Serve app
 const app = express();
 const port = 3000
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/reviews');
+
+// Import Models
+import { Reviews } from './models/Reviews.js'
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -9,12 +21,13 @@ app.use(bodyParser.json())
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/reviews', (req, res) => {
-  // console.log(req.params);
-  // res.send('hello world');
-  // res.send(req.params);
-  let queryParams = req.query;
-  res.send(req.query);
-
+  Reviews.find({_id: req.query.product_id})
+    .then((response) => {
+      res.json(response);
+    })
+    .catch((error) => {
+      res.sendStatus(400);
+    });
 })
 
 app.listen(port, () => {
