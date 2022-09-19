0e1e69da548edd74b14a9eaffbd3954860fd6ee0
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-19 15:34:55 -0700

    feat: replace _id w/ id for reviews_photos.

--- db/transform/transform_reviews_photos.js
+++ db/transform/transform_reviews_photos.js
@@ -1,59 +1,77 @@
-  // Put id and url fields instead photos object
-  db.reviews_photos.aggregate(
-    [
-      {
-        $addFields:
-          {"photo":
-            {
-              id: "$id",
-              url: "$url"
-            }
-          }
-      },
+ // Add indexes to reviews_photos
+ db.reviews_photos.createIndex( { review_id: 1} );
+ db.reviews_photos.createIndex( { id: 1} );
 
-      {
-        $out: "reviews_photos_transformed"
-      }
-    ]
-  );
+// Replace auto generated _id with id
+db.reviews_photos.aggregate([
+  {
+    $addFields: {
+      _id: "$id"
+    }
+  },
 
-  // Remove unnecessary fields
-  db.reviews_photos_transformed.aggregate(
-    [
-      {
-        $project: {
-          _id: 1,
-          review_id: 1,
-          photo: 1
-        }
-      },
+  {
+    $out: "reviews_photos"
+  }
 
-      {
-        $out: "reviews_photos_transformed"
-      }
-    ]
-  );
+]);
 
-
-  // convert photos object to array of review id
-  // Set review_id as _id
-  db.reviews_photos_transformed.aggregate(
-    [
-      {
-        $group:
+// Put id and url fields instead photos object
+db.reviews_photos.aggregate(
+  [
+    {
+      $addFields:
+        {"photo":
           {
-            _id: "$review_id",
-            photos: {
-              $push: "$photo",
-            }
+            id: "$id",
+            url: "$url"
           }
-      },
+        }
+    },
 
-      {
-        $out: "reviews_photos_transformed"
+    {
+      $out: "reviews_photos_transformed"
+    }
+  ]
+);
+
+// Remove unnecessary fields
+db.reviews_photos_transformed.aggregate(
+  [
+    {
+      $project: {
+        _id: 1,
+        review_id: 1,
+        photo: 1
       }
-    ]
-  );
+    },
+
+    {
+      $out: "reviews_photos_transformed"
+    }
+  ]
+);
 
 
-  // db.reviews_photos_transformed.find({_id: 5});
+// convert photos object to array of review id
+// Set review_id as _id
+db.reviews_photos_transformed.aggregate(
+  [
+    {
+      $group:
+        {
+          _id: "$review_id",
+          photos: {
+            $push: "$photo",
+          }
+        }
+    },
+
+    {
+      $out: "reviews_photos_transformed"
+    }
+  ]
+);
+
+
+// db.reviews_photos_transformed.find({_id: 5});
