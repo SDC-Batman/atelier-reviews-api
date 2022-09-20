4da0b7a3afdf92e9a1c5ff5dd1a36ac9bf594062
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-20 14:56:38 -0700

    feat: add npm scripts for loading and transforming data and conducting stress tests.

--- tests/script.js
+++ tests/script.js
@@ -2,29 +2,32 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  vus: 1,
+  // vus: 1,
   thresholds: {
     http_req_duration: ['med<50', 'max<50'],
   },
-  // stages: [
-  //   { duration: '30s', target: 1000 },
-  // ],
-  scenarios: {
-    constant_request_rate: {
-      executor: 'constant-arrival-rate',
-      rate: 1000,
-      timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
-      duration: '30s',
-      preAllocatedVUs: 1, // how large the initial pool of VUs would be
-      // maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
-    },
-  },
+  stages: [
+    { duration: '30s', target: 1000 },
+  ],
+  // scenarios: {
+  //   constant_request_rate: {
+  //     executor: 'constant-arrival-rate',
+  //     rate: 1000,
+  //     timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
+  //     duration: '30s',
+  //     preAllocatedVUs: 1, // how large the initial pool of VUs would be
+  //     // maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
+  //   },
+  // },
 };
+
+// const API_BASE_URL = 'http://localhost:3000/reviews';
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 export default function () {
   const product_id = getRandomInt(1, 1000000);
-  http.get(`http://localhost:3000/reviews/?product_id=${product_id}`);
+  // http.get(`http://localhost:3000/reviews/?product_id=${product_id}`);
+  http.get(`http://localhost:3000/reviews/meta/?product_id=${product_id}`);
   sleep(1);
 }
