55052123b0eaa4280789ce5f622e83cdec65d7f4
Thomas Hepner <hepner.thomas@gmail.com>
2022-09-20 16:16:31 -0700

    feat: modify K6 stress testing script.

--- tests/script.js
+++ tests/script.js
@@ -1,24 +1,16 @@
+// import { URLSearchParams } from 'https://jslib.k6.io/url/1.0.0/index.js';
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { sleep, check } from 'k6';
 
 export const options = {
   // vus: 1,
   thresholds: {
-    http_req_duration: ['med<50', 'max<50'],
+    http_req_duration: ['max<2000'],
+    http_req_failed: ['rate<0.01'],
   },
   stages: [
-    { duration: '30s', target: 1000 },
+    { duration: '30s', target: 100 }, // 1000 per sec
   ],
-  // scenarios: {
-  //   constant_request_rate: {
-  //     executor: 'constant-arrival-rate',
-  //     rate: 1000,
-  //     timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
-  //     duration: '30s',
-  //     preAllocatedVUs: 1, // how large the initial pool of VUs would be
-  //     // maxVUs: 200, // if the preAllocatedVUs are not enough, we can initialize more
-  //   },
-  // },
 };
 
 // const API_BASE_URL = 'http://localhost:3000/reviews';
@@ -27,7 +19,10 @@ const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min)
 
 export default function () {
   const product_id = getRandomInt(1, 1000000);
-  // http.get(`http://localhost:3000/reviews/?product_id=${product_id}`);
-  http.get(`http://localhost:3000/reviews/meta/?product_id=${product_id}`);
+  const response = http.get(`http://localhost:3000/reviews/?product_id=${product_id}`);
+  // http.get(`http://localhost:3000/reviews/meta/?product_id=${product_id}`);
+
+  // per VU, iterations (i.e. requests) per second
+  check(response, { 'status was 200': (r) => r.status === 200 });
   sleep(1);
 }
